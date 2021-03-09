72a553a9a5de55ebf632efad5d3d133628331547
Athena Fernandes Sarantôpoulos <athenasaran@gmail.com>
2021-03-09 12:38:37 -0300

    highscore

--- end.js
+++ end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById('saveScoreBtn')
 const finalScore = document.getElementById('finalScore')
 const mostRecentScore = localstorage.getItem('mostRecentScore')
 
+const highScore = JSON.parse(localstorage.getItem('highScore')) || []
+
+const MAX_HIGH_SCORES = 5
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -11,4 +15,18 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    }
+    highScore.push(score)
+
+    highScore.sort((a, b) => b.score - a.score)
+
+    highScore.splice(5)
+
+    localStorage.setItem('highScore', JSON.stringify(highScores))
+    window.location.assign('/')
+
 }
